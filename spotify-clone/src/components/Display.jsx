19f2c3da949959f71aa489fee8af6b82b3923b05
Rajesh97266 <rajesh97266@gmail.com>
2025-04-08 13:21:14 +0530

    display-routes-added

--- spotify-clone/src/components/Display.jsx
+++ spotify-clone/src/components/Display.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import DisplayHome from "./DisplayHome";
+import DisplayAlbum from "./DisplayAlbum";
 
-const Display = () => {
+function Display() {
   return (
-    <div>Display</div>
-  )
+    <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
+      <Routes>
+        <Route path="/" element={<DisplayHome />} />
+        <Route path="/album/:id" element={<DisplayAlbum />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default Display
+export default Display;
---
+++ spotify-clone/src/components/DisplayAlbum.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DisplayAlbum = () => {
+  return (
+    <div>DisplayAlbum</div>
+  )
+}
+
+export default DisplayAlbum
